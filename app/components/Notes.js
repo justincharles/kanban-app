5a041a38a1215d67678c9699d134e729e4f92e43
Justin Charles <justinrobertcharles@gmail.com>
2016-08-14 03:22:07 -0400

    Editable notes, basic styles

--- app/components/Notes.js
+++ app/components/Notes.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import Note from './Note';
+import Editable from './Editable';
 
-export default ({notes, onDelete = () => {}}) => (
-  <ul>{notes.map(({id, task}) =>
-    <li key={id}>
-      <Note
-        onDelete={onDelete.bind(null, id)}
-        task={task}
-      />
-    </li>
+export default ({
+  notes,
+  onNoteClick = () => {}, onEdit = () => {}, onDelete = () => {}
+}) => (
+  <ul className='notes'>
+    {notes.map(({id, editing, task}) =>
+      <li key={id}>
+        <Note className='note' onClick={onNoteClick.bind(null, id)}>
+          <Editable className='editable'
+            editing={editing}
+            value={task}
+            onEdit={onEdit.bind(null, id)} />
+          <button
+            className='delete'
+            onClick={onDelete.bind(null, id)}>x</button>
+        </Note>
+      </li>
     )}
   </ul>
 );
